3d1d7d099413bd59983f96ef162b3299ea360ce5
AdamMahmoud <adammahmoud@AdamMahmouds-MacBook-Pro.local>
2024-10-25 00:17:04 -0400

    update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(session({ secret: 'secret-key', resave: false, saveUninitialized: false 
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 // Homepage Route
 app.get('/', (req, res) => {
   res.render('index');
@@ -55,8 +55,9 @@ app.get('/', (req, res) => {
 //   }
 // });
 
-app.get("/register", (req, res) => {
-    res.render('register');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/register.ejs");
+
   });
 
   app.post('/register', async (req, res) => {
